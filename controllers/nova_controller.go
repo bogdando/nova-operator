513d5db046557e5b863fdd5124d6f43b2540f079
Balazs Gibizer <gibi@redhat.com>
2022-12-18 16:32:13 +0100

    Make TransportURL name depend on  Nova CR name
    So far nova-api-transport as TransportURL name was hardcoded. Also the
    cell TransportURL names only depended on the name of the cell but
    did not depend on the name of the Nova CR. Now both issue if fixed.

--- controllers/nova_controller.go
+++ controllers/nova_controller.go
@@ -251,7 +251,7 @@ func (r *NovaReconciler) Reconcile(ctx context.Context, req ctrl.Request) (resul
 	// message bus is always the same as the top level API message bus so
 	// we create API MQ separately first
 	apiMQSecretName, apiMQStatus, apiMQError := r.ensureMQ(
-		ctx, h, instance, "nova-api-transport", instance.Spec.APIMessageBusInstance)
+		ctx, h, instance, instance.Name+"-api-transport", instance.Spec.APIMessageBusInstance)
 	switch apiMQStatus {
 	case nova.MQFailed:
 		instance.Status.Conditions.Set(condition.FalseCondition(
@@ -291,7 +291,7 @@ func (r *NovaReconciler) Reconcile(ctx context.Context, req ctrl.Request) (resul
 			err = apiMQError
 		} else {
 			cellMQ, status, err = r.ensureMQ(
-				ctx, h, instance, cellName+"-transport", cellTemplate.CellMessageBusInstance)
+				ctx, h, instance, instance.Name+"-"+cellName+"-transport", cellTemplate.CellMessageBusInstance)
 		}
 		switch status {
 		case nova.MQFailed:
